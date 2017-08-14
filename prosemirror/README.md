a9eeb748402ea087b0e774875637bba93f69573b
Shreevatsa R <shreevatsa.public@gmail.com>
2017-08-13 20:20:20 -0700

    Adding menu bar

--- prosemirror/README.md
+++ prosemirror/README.md
@@ -66,14 +66,18 @@ From Javascript (or the browser console), you can get the document contents with
 
 After a few minutes of playing with the above, you may notice a few problems:
 
-- There is no way to enter new paragraphs, apart from pasting them in. Solution: ?
+- There is no way to enter new paragraphs, apart from pasting them in.
 
-- Related: If you make a paragraph empty by deleting all the text in it, there is no way to delete the paragraph itself. Solution: ?
+- Related: If you make a paragraph empty by deleting all the text in it, there is no way to delete the paragraph itself.
 
 - It would be nice to have a richer schema (headings, bold, ... ?) and a correspondingly richer view, more editor features like undo, etc.
 
 Let's look at these, in a random order:
 
-- Adding undo is documented in the ProseMirror docs, so it turns out to be quite easy to do: see commit 3c6f86ae6f214232ac7c75e720971b3c7ea59695.
+- Adding undo is documented in the ProseMirror docs, so it turns out to be quite easy to do: see commit 3c6f86ae6f214232ac7c75e720971b3c7ea59695 which basically adds
 
-- Let's make it possible to hit Enter to start a new paragraph.
+        plugins: [history.history(), keymap({"Mod-z": history.undo})]
+
+- Let's make it possible to hit Enter to start a new paragraph: see 7c58f8c2f3fabb6cd8fc9b76265a1a9527725eca which adds `keymap(baseKeymap)` to `plugins`, which is defined in [prosemirror-commands](https://github.com/ProseMirror/prosemirror-commands/blob/52b3a1baddf4bb3f956ceb43b4000d4bc44c049b/src/commands.js). This is a bunch of commonsensical actions for individual keystrokes that users expect anyway.
+
+- Let's add a menu for usability (which will gain more entries as we update our schema): see
--- prosemirror/next.js
+++ prosemirror/next.js
@@ -5,6 +5,68 @@ const history = require("prosemirror-history")
 const {keymap} = require("prosemirror-keymap")
 // const {inputRules, allInputRules} = require("prosemirror-inputrules")
 const {baseKeymap} = require("prosemirror-commands")
+const {menuBar, blockTypeItem, Dropdown, joinUpItem, liftItem, selectParentNodeItem, undoItem, redoItem} = require("prosemirror-menu")
+
+// Copied(!) from https://github.com/ProseMirror/prosemirror-example-setup/blob/44a2fd4/src/menu.js with some clearly irrelevant bits removed
+function buildMenuItems(schema) {
+  let r = {}, type
+  if (type = schema.marks.strong)
+    r.toggleStrong = markItem(type, {title: "Toggle strong style", icon: icons.strong})
+  if (type = schema.marks.em)
+    r.toggleEm = markItem(type, {title: "Toggle emphasis", icon: icons.em})
+
+  if (type = schema.nodes.bullet_list)
+    r.wrapBulletList = wrapListItem(type, {
+      title: "Wrap in bullet list",
+      icon: icons.bulletList
+    })
+  if (type = schema.nodes.ordered_list)
+    r.wrapOrderedList = wrapListItem(type, {
+      title: "Wrap in ordered list",
+      icon: icons.orderedList
+    })
+  if (type = schema.nodes.blockquote)
+    r.wrapBlockQuote = wrapItem(type, {
+      title: "Wrap in block quote",
+      icon: icons.blockquote
+    })
+  if (type = schema.nodes.paragraph)
+    r.makeParagraph = blockTypeItem(type, {
+      title: "Change to paragraph",
+      label: "Plain"
+    })
+  if (type = schema.nodes.heading)
+    for (let i = 1; i <= 10; i++)
+      r["makeHead" + i] = blockTypeItem(type, {
+        title: "Change to heading " + i,
+        label: "Level " + i,
+        attrs: {level: i}
+      })
+  if (type = schema.nodes.horizontal_rule) {
+    let hr = type
+    r.insertHorizontalRule = new MenuItem({
+      title: "Insert horizontal rule",
+      label: "Horizontal rule",
+      select(state) { return canInsert(state, hr) },
+      run(state, dispatch) { dispatch(state.tr.replaceSelectionWith(hr.create())) }
+    })
+  }
+  let cut = arr => arr.filter(x => x)
+  r.insertMenu = new Dropdown(cut([r.insertImage, r.insertHorizontalRule, r.insertTable]), {label: "Insert"})
+  r.typeMenu = new Dropdown(cut([r.makeParagraph, r.makeCodeBlock, r.makeHead1 && new DropdownSubmenu(cut([
+    r.makeHead1, r.makeHead2, r.makeHead3, r.makeHead4, r.makeHead5, r.makeHead6
+  ]), {label: "Heading"})]), {label: "Type..."})
+  let tableItems = cut([r.addRowBefore, r.addRowAfter, r.removeRow, r.addColumnBefore, r.addColumnAfter, r.removeColumn])
+  if (tableItems.length)
+    r.tableMenu = new Dropdown(tableItems, {label: "Table"})
+
+  r.inlineMenu = [cut([r.toggleStrong, r.toggleEm, r.toggleCode, r.toggleLink]), [r.insertMenu]]
+  r.blockMenu = [cut([r.typeMenu, r.tableMenu, r.wrapBulletList, r.wrapOrderedList, r.wrapBlockQuote, joinUpItem,
+                      liftItem, selectParentNodeItem])]
+  r.fullMenu = r.inlineMenu.concat(r.blockMenu).concat([[undoItem, redoItem]])
+
+  return r
+}
 
 const trivialSchema = new Schema({
     nodes: {
@@ -26,8 +88,10 @@ window.view = new EditorView(document.body, {
             "Mod-z": history.undo,
             "Mod-Shift-z": history.redo
             }),
-            // inputRules({rules: allInputRules}),
+            // inputRules({rules: allInputRules}), // emDash, ellipsis, smartQuotes -- not worth it?
             keymap(baseKeymap),
+            menuBar({floating: true,
+                     content: buildMenuItems(trivialSchema).fullMenu}),
         ],
     }),
 })
